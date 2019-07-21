89299b2ca2ef500de61af3dfc66ef146674c8e8d
Tyr Mactire <tyr@pettingzoo.co>
2019-07-20 23:44:24 -0700

    Draw Pixel

---
+++ gfxos/matrix.go
@@ -0,0 +1,86 @@
+package gfxos
+
+import (
+	"fmt"
+	"github.com/jacobsa/go-serial/serial"
+	"io"
+	"log"
+	"sync"
+)
+
+type Matrix struct {
+	SPort *io.ReadWriteCloser
+	Wmux sync.Mutex
+}
+
+
+func Open(portName string) (*Matrix, error) {
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName: "COM30",
+		BaudRate: 19200,
+		DataBits: 8,
+		StopBits: 1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create new Matrix
+	newMatrix := Matrix{}
+	newMatrix.SPort = &port
+
+	return &newMatrix, nil
+}
+
+func (m Matrix) Close() () {
+	port := *m.SPort
+	port.Close()
+}
+func (m *Matrix) DrawPixel(x int, y int, r int, g int, b int) error {
+	color := color888(r, g, b)
+
+	err := m.write(fmt.Sprintf("01%02x%02x%04x", x, y, color))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Matrix) write(text string) error {
+	b := []byte(text + "\n")
+	fmt.Println(b)
+
+	// Write bytes to the port.
+	m.Wmux.Lock()
+	port := *m.SPort
+	_, err := port.Write(b)
+	m.Wmux.Unlock()
+
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
+
+	return nil
+}
+
+func color888(r int, g int, b int) int {
+	new_r := mapRange(r, 0, 255, 0, 31)
+	new_g := mapRange(g, 0, 255, 0, 63)
+	new_b := mapRange(b, 0, 255, 0, 31)
+
+	new_g = new_g << 5
+	new_r = new_r << 11
+
+	color := new_r | new_g | new_b
+	return color
+}
+
+func mapRange(value int, fromLow int, fromHigh int, toLow int, toHigh int) int {
+	return (((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow)) + toLow
+}
---
+++ gfxos/matrix_test.go
@@ -0,0 +1,85 @@
+package gfxos
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+type testColor888 struct {
+	red int
+	green int
+	blue int
+	value int
+}
+
+var testColor888s = []testColor888{
+	{ 255,0,0,0xfe00},
+	{ 0,255,0,0x7e0},
+	{ 0,0,255,0x1f},
+	{ 255,255,0,0xffe0},
+	{ 0,255,255,0x7ff},
+	{ 255,0,255,0xf81f},
+	{ 255,255,255,0xffff},
+}
+
+func TestColr888(t *testing.T) {
+	for _, testVal := range testColor888s {
+
+		v := color888(testVal.red, testVal.green, testVal.blue)
+		if v != testVal.value {
+			t.Error(
+				"For [",
+				fmt.Sprintf("%x", testVal.red),
+				fmt.Sprintf("%x", testVal.green),
+				fmt.Sprintf("%x", testVal.blue),
+				"] expected",
+				fmt.Sprintf("%x", testVal.value),
+				"got",
+				fmt.Sprintf("%x", v),
+			)
+		}
+	}
+}
+
+type testmap struct {
+	fromLow int
+	fromHigh int
+	toLow int
+	toHigh int
+	value int
+	result int
+}
+
+var testMapRanges = []testmap{
+	{ 0,255,0,31,0,0},
+	{ 0,255,0,31,63,7},
+	{ 0,255,0,31,127,15},
+	{ 0,255,0,31,171,20},
+	{ 0,255,0,31,246,29},
+	{ 0,255,0,31,247,30},
+	{ 0,255,0,31,254,30},
+	{ 0,255,0,31,255,31},
+	{ 0,255,0,127,0,0},
+	{ 0,255,0,127,63,31},
+	{ 0,255,0,127,127,63},
+	{ 0,255,0,127,171,85},
+	{ 0,255,0,127,246,122},
+	{ 0,255,0,127,247,123},
+	{ 0,255,0,127,254,126},
+	{ 0,255,0,127,255,127},
+}
+
+func TestMapRange(t *testing.T) {
+	for _, testVal := range testMapRanges {
+
+		v := mapRange(testVal.value, testVal.fromLow, testVal.fromHigh, testVal.toLow, testVal.toHigh)
+		if v != testVal.result {
+			t.Error(
+				"For", testVal.value,
+				"expected", testVal.result,
+				"got", v,
+			)
+		}
+	}
+}
